d43584316c8e407410dda3b8b000a6214587f1ac
liupu <575866447@qq.com>
2019-12-24 22:34:32 +0800

    Resource

--- lib_uglide/src/main/java/com/update/lib_uglide/recycle/Resource.java
+++ lib_uglide/src/main/java/com/update/lib_uglide/recycle/Resource.java
@@ -1,5 +1,9 @@
 package com.update.lib_uglide.recycle;
 
+import android.graphics.Bitmap;
+
+import com.update.lib_uglide.cache.Key;
+
 /**
  * @author : liupu
  * date   : 2019/12/24
@@ -7,4 +11,47 @@ package com.update.lib_uglide.recycle;
  * github : https://github.com/CodeLiuPu/
  */
 public class Resource {
+
+    private Bitmap bitmap;
+
+    /**
+     * 引用计数
+     * 当为0时, 回调ResourceListener
+     * 将图片存入内存缓存
+     */
+    private int acquired;
+
+    private Key key;
+
+    private ResourceListener listener;
+
+    public interface ResourceListener {
+        void onResourceReleased(Key key, Resource resource);
+    }
+
+    public Resource(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public void setResourceListener(Key key, ResourceListener listener) {
+        this.key = key;
+        this.listener = listener;
+    }
+
+    public void acquire() {
+        if (bitmap.isRecycled()) {
+            throw new IllegalStateException("Cannot acquire a recycled resource");
+        }
+        ++acquired;
+    }
+
+    public void release() {
+        if (--acquired == 0) {
+            listener.onResourceReleased(key, this);
+        }
+    }
 }
---
+++ lib_uglide/src/main/java/com/update/lib_uglide/request/Request.java
@@ -0,0 +1,11 @@
+package com.update.lib_uglide.request;
+
+/**
+ * @author : liupu
+ * date    : 2019/12/24
+ * desc    :
+ * github : https://github.com/CodeLiuPu/
+ */
+public class Request {
+
+}
