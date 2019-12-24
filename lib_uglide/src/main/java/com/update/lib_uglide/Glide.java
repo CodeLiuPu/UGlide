755d2a8e870b6f42f1f937a4a7f4cda516050216
liupu <575866447@qq.com>
2019-12-24 17:49:55 +0800

    Glide

---
+++ lib_uglide/src/main/java/com/update/lib_uglide/Glide.java
@@ -0,0 +1,29 @@
+package com.update.lib_uglide;
+
+import android.content.ComponentCallbacks2;
+import android.content.res.Configuration;
+
+/**
+ * @author : liupu
+ * date   : 2019/12/24
+ * desc   :
+ * github : https://github.com/CodeLiuPu/
+ */
+public class Glide implements ComponentCallbacks2 {
+
+    
+    @Override
+    public void onTrimMemory(int level) {
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+
+    }
+
+    @Override
+    public void onLowMemory() {
+
+    }
+}
