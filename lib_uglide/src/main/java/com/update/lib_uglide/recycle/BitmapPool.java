4f62ebc736610a4e848d154c624adf6e05c4b8c9
liupu <575866447@qq.com>
2019-12-24 18:11:33 +0800

    BitmapPool

---
+++ lib_uglide/src/main/java/com/update/lib_uglide/recycle/BitmapPool.java
@@ -0,0 +1,20 @@
+package com.update.lib_uglide.recycle;
+
+import android.graphics.Bitmap;
+
+/**
+ * @author : liupu
+ * date   : 2019/12/24
+ * desc   : 复用池
+ * github : https://github.com/CodeLiuPu/
+ */
+public interface BitmapPool {
+    
+    void put(Bitmap bitmap);
+
+    Bitmap get(int width, int height, Bitmap.Config config);
+
+    void clearMemory();
+
+    void trimMemory();
+}
