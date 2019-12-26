2e4d04bf93041f9a06e1d76066ee60e60d227b5b
liupu <575866447@qq.com>
2019-12-26 14:33:25 +0800

    EngineKey ObjectKey

--- lib_uglide/src/main/java/com/update/lib_uglide/load/EngineKey.java
+++ lib_uglide/src/main/java/com/update/lib_uglide/load/EngineKey.java
@@ -12,14 +12,61 @@ import java.security.MessageDigest;
  */
 public class EngineKey implements Key {
 
+    private final Object model;
+    private final int width;
+    private final int height;
+    private int hashCode;
+
+    public EngineKey(Object model, int width, int height) {
+        this.model = model;
+        this.width = width;
+        this.height = height;
+    }
 
     @Override
     public void updateDiskCacheKey(MessageDigest messageDigest) {
-
+        messageDigest.update(getKeyBytes());
     }
 
     @Override
     public byte[] getKeyBytes() {
-        return new byte[0];
+        return toString().getBytes();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null && getClass() != obj.getClass()) {
+            return false;
+        }
+        EngineKey engineKey = (EngineKey) obj;
+        if (width != engineKey.width
+                || height != engineKey.height
+                || hashCode != engineKey.hashCode) {
+            return false;
+        }
+        return model != null ? model.equals(engineKey.model) : engineKey.model == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = model != null ? model.hashCode() : 0;
+        result = 31 * result + width;
+        result = 31 * result + height;
+        result = 31 * result + hashCode;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "EngineKey{" +
+                "model=" + model +
+                ", width=" + width +
+                ", height=" + height +
+                ", hashCode=" + hashCode +
+                '}';
     }
 }
--- lib_uglide/src/main/java/com/update/lib_uglide/load/ObjectKey.java
+++ lib_uglide/src/main/java/com/update/lib_uglide/load/ObjectKey.java
@@ -12,14 +12,37 @@ import java.security.MessageDigest;
  */
 public class ObjectKey implements Key {
 
+    private final Object model;
+
+    public ObjectKey(Object model) {
+        this.model = model;
+    }
 
     @Override
     public void updateDiskCacheKey(MessageDigest messageDigest) {
-
+        messageDigest.update(model.toString().getBytes());
     }
 
     @Override
     public byte[] getKeyBytes() {
         return new byte[0];
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        ObjectKey objectKey = (ObjectKey) object;
+
+        return model != null ? model.equals(objectKey.model) : objectKey.model == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return model != null ? model.hashCode() : 0;
     }
 }
